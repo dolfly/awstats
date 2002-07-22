f7072bf71aa838a55f93824880fc6b7e5678ec2a
Laurent Destailleur <eldy@users.sourceforge.net>
2002-07-22 18:50:45 +0000

    Added Plugins support

---
+++ wwwroot/cgi-bin/plugins/readgz.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+#-----------------------------------------------------------------------------
+# ReadGZ AWStats plugin
+# Allow AWStats to read gz files
+#-----------------------------------------------------------------------------
+# Perl Required Modules: Compress::Zlib
+#-----------------------------------------------------------------------------
+# $Revision: 1.1 $ - $Author: eldy $ - $Date: 2002-07-22 18:50:45 $
+
+use Compress::Zlib;
+
+
+$PluginCompress=1;
+
+1;	# Do not remove this line
