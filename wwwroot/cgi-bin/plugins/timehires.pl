363176be8e88f8146be353e3e30b84653e7eb6db
Laurent Destailleur <eldy@users.sourceforge.net>
2002-07-22 18:04:42 +0000

    Added Plugins support

--- wwwroot/cgi-bin/plugins/timehires.pl
+++ wwwroot/cgi-bin/plugins/timehires.pl
@@ -5,11 +5,11 @@
 #-----------------------------------------------------------------------------
 # Perl Required Modules: Time::HiRes
 #-----------------------------------------------------------------------------
-# $Revision: 1.1 $ - $Author: eldy $ - $Date: 2002-07-17 21:10:01 $
+# $Revision: 1.2 $ - $Author: eldy $ - $Date: 2002-07-22 18:04:42 $
 
 use Time::HiRes qw( gettimeofday );
 
 
-$UseHiRes=1;
+$PluginTimeHiRes=1;
 
 1;	# Do not remove this line
