4bbb898c1f9c365d611d8deb9c3f4da4dd3c6fcf
Laurent Destailleur <eldy@users.sourceforge.net>
2003-09-14 17:47:13 +0000

    mod_deflate reported bandwith saved instead of bandwidth used.

--- wwwroot/cgi-bin/awstats.pl
+++ wwwroot/cgi-bin/awstats.pl
@@ -6,7 +6,7 @@
 # line or a browser to read report results.
 # See AWStats documentation (in docs/ directory) for all setup instructions.
 #-----------------------------------------------------------------------------
-# $Revision: 1.588 $ - $Author: eldy $ - $Date: 2003-09-14 14:36:24 $
+# $Revision: 1.589 $ - $Author: eldy $ - $Date: 2003-09-14 17:47:13 $
 
 #use warnings;		# Must be used in test mode only. This reduce a little process speed
 #use diagnostics;	# Must be used in test mode only. This reduce a lot of process speed
@@ -20,7 +20,7 @@ use Socket;
 # Defines
 #-----------------------------------------------------------------------------
 use vars qw/ $REVISION $VERSION /;
-$REVISION='$Revision: 1.588 $'; $REVISION =~ /\s(.*)\s/; $REVISION=$1;
+$REVISION='$Revision: 1.589 $'; $REVISION =~ /\s(.*)\s/; $REVISION=$1;
 $VERSION="5.8 (build $REVISION)";
 
 # ----- Constants -----
@@ -5852,8 +5852,9 @@ if ($UpdateStats && $FrameName ne 'index' && $FrameName ne 'mainleft') {	# Updat
 		$_filetypes_h{$extension}++;
 		$_filetypes_k{$extension}+=int($field[$pos_size]);	# TODO can cause a warning
 		# Compression
-		if ($pos_compratio>=0 && ($field[$pos_compratio] =~ /(\d+)/)) { # Calculate in/out size from percentage
-			$_filetypes_gz_in{$extension}+=int($field[$pos_size]*100/((100-$1)||1));
+		if ($pos_compratio>=0 && ($field[$pos_compratio] =~ /(\d+)/)) { # Calculate in/out size from percentage (% is size after/before)
+			#$_filetypes_gz_in{$extension}+=int($field[$pos_size]*100/((100-$1)||1));
+			$_filetypes_gz_in{$extension}+=int($field[$pos_size]*100/($1||1));
 			$_filetypes_gz_out{$extension}+=int($field[$pos_size]);
 		}
 		elsif ($pos_gzipin>=0 && $field[$pos_gzipin]) {	# If in and out in log
