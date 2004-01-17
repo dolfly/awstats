836d63e59eeb103d7228928ed88c047f5d02da26
Laurent Destailleur <eldy@users.sourceforge.net>
2004-01-17 02:19:42 +0000

    Renamed "nimba" to real name "nimda"

--- wwwroot/cgi-bin/lib/worms.pm
+++ wwwroot/cgi-bin/lib/worms.pm
@@ -3,7 +3,7 @@
 # If you want to add worms to extend AWStats database detection capabilities,
 # you must add an entry in WormsSearchIDOrder, WormsHashID and WormsHashLib.
 #-----------------------------------------------------------------------------
-# $Revision: 1.7 $ - $Author: eldy $ - $Date: 2004-01-04 19:11:38 $
+# $Revision: 1.8 $ - $Author: eldy $ - $Date: 2004-01-17 02:19:42 $
 
 
 #package AWSWORMS;
@@ -34,12 +34,12 @@
 %WormsHashID = (
 '\/default\.ida','code_red',
 '\/null\.idq','code_red',
-'exe\?\/c\+dir','nimba',
-'root\.exe','nimba',
-'admin\.dll','nimba',
+'exe\?\/c\+dir','nimda',
+'root\.exe','nimda',
+'admin\.dll','nimda',
 '\/nsiislog\.dll','mpex',
 '\/sumthin','sumthin',
-'\/winnt\/system32\/cmd\.exe','nimba',
+'\/winnt\/system32\/cmd\.exe','nimda',
 '\/_vti_inf\.html','unknown',
 '\/_vti_bin\/shtml\.exe\/_vti_rpc','unknown'
 #'/MSOffice/cltreq.asp'		# Not a worm, a check by IE to see if discussion bar is turned on
@@ -53,7 +53,7 @@
 %WormsHashLib = (
 'code_red','Code Red family worm',
 'mpex','IIS Exploit worm',
-'nimba','Nimba family worm',
+'nimda','Nimda family worm',
 'sumthin','Sumthin worm',
 'unknown','Unknown worm'
 );
