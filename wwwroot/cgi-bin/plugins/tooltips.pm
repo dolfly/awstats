bc6f3d7c1b43357fc8517c5303053d888309c2cb
Laurent Destailleur <eldy@users.sourceforge.net>
2004-03-04 13:06:15 +0000

    Added worm target in AWStats database.

--- wwwroot/cgi-bin/plugins/tooltips.pm
+++ wwwroot/cgi-bin/plugins/tooltips.pm
@@ -7,7 +7,7 @@
 #-----------------------------------------------------------------------------
 # Perl Required Modules: None
 #-----------------------------------------------------------------------------
-# $Revision: 1.10 $ - $Author: eldy $ - $Date: 2004-03-01 20:49:38 $
+# $Revision: 1.11 $ - $Author: eldy $ - $Date: 2004-03-04 13:06:15 $
 
 
 # <-----
@@ -185,6 +185,7 @@ sub _ReadAndOutputTooltipFile {
 		my $aws_PROG=ucfirst($PROG);
 		my $aws_VisitTimeout = $VISITTIMEOUT/10000*60;
 		my $aws_NbOfRobots = scalar keys %RobotsHashIDLib;
+		my $aws_NbOfWorms = scalar @WormsSearchIDOrder;
 		my $aws_NbOfSearchEngines = scalar keys %SearchEnginesHashLib;
 		while (<LANG>) {
 			if ($_ =~ /\<!--/) { next; }	# Remove comment
@@ -193,6 +194,7 @@ sub _ReadAndOutputTooltipFile {
 			s/#MaxNbOfRefererShown#/$MaxNbOf{'RefererShown'}/;
 			s/#VisitTimeOut#/$aws_VisitTimeout/;
 			s/#RobotArray#/$aws_NbOfRobots/;
+			s/#WormsArray#/$aws_NbOfWorms/;
 			s/#SearchEnginesArray#/$aws_NbOfSearchEngines/;
 			print "$_";
 		}
