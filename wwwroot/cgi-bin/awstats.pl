f95cdf61cbffc027ddbe99ba84f91dd7aa772073
Laurent Destailleur <eldy@users.sourceforge.net>
2004-08-01 16:23:42 +0000

    Updated documentation.

--- wwwroot/cgi-bin/awstats.pl
+++ wwwroot/cgi-bin/awstats.pl
@@ -6,7 +6,7 @@
 # line or a browser to read report results.
 # See AWStats documentation (in docs/ directory) for all setup instructions.
 #------------------------------------------------------------------------------
-# $Revision: 1.773 $ - $Author: eldy $ - $Date: 2004-08-01 15:17:52 $
+# $Revision: 1.774 $ - $Author: eldy $ - $Date: 2004-08-01 16:23:42 $
 require 5.005;
 
 #$|=1;
@@ -21,7 +21,7 @@ use Socket;
 # Defines
 #------------------------------------------------------------------------------
 use vars qw/ $REVISION $VERSION /;
-$REVISION='$Revision: 1.773 $'; $REVISION =~ /\s(.*)\s/; $REVISION=$1;
+$REVISION='$Revision: 1.774 $'; $REVISION =~ /\s(.*)\s/; $REVISION=$1;
 $VERSION="6.2 (build $REVISION)";
 
 # ----- Constants -----
@@ -5460,9 +5460,9 @@ if ($ENV{'AWSTATS_FORCE_CONFIG'}) {
 if ((! $ENV{'GATEWAY_INTERFACE'}) && (! $SiteConfig)) {
 	&Read_Ref_Data('browsers','domains','operating_systems','robots','search_engines','worms');
 	print "----- $PROG $VERSION (c) 2000-2004 Laurent Destailleur -----\n";
-	print "$PROG is a free web server logfile analyzer to show you advanced web\n";
+	print "AWStats is a free web server logfile analyzer to show you advanced web\n";
 	print "statistics.\n";
-	print "$PROG comes with ABSOLUTELY NO WARRANTY. It's a free software distributed\n";
+	print "AWStats comes with ABSOLUTELY NO WARRANTY. It's a free software distributed\n";
 	print "with a GNU General Public License (See LICENSE file for details).\n";
 	print "\n";
 	print "Syntax: $PROG.$Extension -config=virtualhostname [options]\n";
