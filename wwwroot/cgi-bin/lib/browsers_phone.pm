c64b7c4fb16b58597d4fa1f8a0d6e1b1e9b1b55c
Laurent Destailleur <eldy@users.sourceforge.net>
2005-03-05 19:37:43 +0000

    Add browsers_phone.pm

--- wwwroot/cgi-bin/lib/browsers_phone.pm
+++ wwwroot/cgi-bin/lib/browsers_phone.pm
@@ -3,7 +3,8 @@
 # If you want to add a Browser to extend AWStats database detection capabilities,
 # you must add an entry in BrowsersSearchIDOrder and in BrowsersHashIDLib.
 #-------------------------------------------------------
-# $ - $Author: eldy $ - $Date: 2005-03-04 21:55:38 $
+# Originale version by malu
+# $Revision: 1.2 $ - $Author: eldy $ - $Date: 2005-03-05 19:37:43 $
 
 
 #package AWSUA;
