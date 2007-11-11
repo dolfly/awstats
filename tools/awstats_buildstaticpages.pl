d4780d96ec87230f99486271f6e310107119e24d
Laurent Destailleur <eldy@users.sourceforge.net>
2007-11-11 19:40:43 +0000

    New: Can show a full list for extrasection

--- tools/awstats_buildstaticpages.pl
+++ tools/awstats_buildstaticpages.pl
@@ -3,7 +3,7 @@
 # Launch awstats with -staticlinks option to build all static pages.
 # See COPYING.TXT file about AWStats GNU General Public License.
 #------------------------------------------------------------------------------
-# $Revision: 1.35 $ - $Author: eldy $ - $Date: 2007-08-30 16:43:15 $
+# $Revision: 1.36 $ - $Author: eldy $ - $Date: 2007-11-11 19:40:33 $
 
 #$|=1;
 #use warnings;		# Must be used in test mode only. This reduce a little process speed
@@ -15,7 +15,7 @@ use Time::Local;	# use Time::Local 'timelocal_nocheck' is faster but not support
 #------------------------------------------------------------------------------
 # Defines
 #------------------------------------------------------------------------------
-my $REVISION='$Revision: 1.35 $'; $REVISION =~ /\s(.*)\s/; $REVISION=$1;
+my $REVISION='$Revision: 1.36 $'; $REVISION =~ /\s(.*)\s/; $REVISION=$1;
 my $VERSION="1.2 (build $REVISION)";
 
 # ---------- Init variables --------
@@ -55,7 +55,9 @@ $ShowRobotsStats $ShowSessionsStats $ShowPagesStats $ShowFileTypesStats
 $ShowOSStats $ShowBrowsersStats $ShowOriginStats
 $ShowKeyphrasesStats $ShowKeywordsStats $ShowMiscStats $ShowHTTPErrorsStats
 $BuildReportFormat
+@ExtraName
 /;
+@ExtraName = ();
 # ----- Time vars -----
 use vars qw/
 $starttime
@@ -223,6 +225,9 @@ sub Parse_Config {
 			while ($value =~ /__([^\s_]+(?:_[^\s_]+)*)__/) { my $var=$1; $value =~ s/__${var}__/$ENV{$var}/g; }
 		}
 
+		# Extra parameters
+ 		if ($param =~ /^ExtraSectionName(\d+)/)			{ $ExtraName[$1]=$value; next; }
+
 		# If parameters was not found previously, defined variable with name of param to value
 		$$param=$value;
 	}
@@ -354,6 +359,9 @@ if ($ShowHTTPErrorsStats) {
 	push @OutputList,'errors404';		
 }
 #if ($ShowSMTPErrorsStats) { push @OutputList,'errors'; }
+foreach my $extranum (1..@ExtraName-1) {
+	push @OutputList,'allextra'.$extranum;
+}
 
 # Launch awstats update
 if ($Update) {
--- wwwroot/cgi-bin/awstats.pl
+++ wwwroot/cgi-bin/awstats.pl
@@ -6,7 +6,7 @@
 # line or a browser to read report results.
 # See AWStats documentation (in docs/ directory) for all setup instructions.
 #------------------------------------------------------------------------------
-# $Revision: 1.898 $ - $Author: eldy $ - $Date: 2007-10-12 22:04:33 $
+# $Revision: 1.899 $ - $Author: eldy $ - $Date: 2007-11-11 19:40:43 $
 require 5.005;
 
 #$|=1;
@@ -21,7 +21,7 @@ use Socket;
 # Defines
 #------------------------------------------------------------------------------
 use vars qw/ $REVISION $VERSION /;
-$REVISION='$Revision: 1.898 $'; $REVISION =~ /\s(.*)\s/; $REVISION=$1;
+$REVISION='$Revision: 1.899 $'; $REVISION =~ /\s(.*)\s/; $REVISION=$1;
 $VERSION="6.8 (build $REVISION)";
 
 # ----- Constants -----
@@ -1442,11 +1442,14 @@ sub Parse_Config {
  		if ($param =~ /^ExtraSectionAddSumRow(\d+)/) 	{ $ExtraAddSumRow[$1]=$value; next; }
  		if ($param =~ /^MaxNbOfExtra(\d+)/) 			{ $MaxNbOfExtra[$1]=$value; next; }
  		if ($param =~ /^MinHitExtra(\d+)/) 				{ $MinHitExtra[$1]=$value; next; }
-		# Special appearance parameters
+
+		# Plugins
 		if ($param =~ /^LoadPlugin/)          			{ push @PluginsToLoad, $value; next; }
+
 		# Other parameter checks we need to put after MaxNbOfExtra and MinHitExtra
  		if ($param =~ /^MaxNbOf(\w+)/) 	{ $MaxNbOf{$1}=$value; next; }
  		if ($param =~ /^MinHit(\w+)/) 	{ $MinHit{$1}=$value; next; }
+
 		# Check if this is a known parameter
 #		if (! $ConfOk{$param}) { error("Unknown config parameter '$param' found line $conflinenb in file \"configFile\""); }
 		# If parameters was not found previously, defined variable with name of param to value
@@ -5779,6 +5782,7 @@ if ((! $ENV{'GATEWAY_INTERFACE'}) && (! $SiteConfig)) {
 	print "               keyphrases       to list all keyphrases used on search engines\n";
 	print "               keywords         to list all keywords used on search engines\n";
 	print "               errors404        to list 'Referers' for 404 errors\n";
+	print "               allextraX        to build page of all values for ExtraSection X\n";
 	print "  -staticlinks to have static links in HTML report page\n";
 	print "  -staticlinksext=xxx to have static links with .xxx extension instead of .html\n";
 	print "  -lang=LL     to output a HTML report in language LL (en,de,es,fr,it,nl,...)\n";
